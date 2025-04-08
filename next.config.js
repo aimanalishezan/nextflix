3ec3f2e6fa44f32adaa1f4130657dbbfee381522
aimanalishezan <aimanalishezan22@gmail.com>
2025-04-08 15:31:33 +0600

    hi

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    disableStaticImages: false,
+  },
+  webpack(config) {
+    // Add support for importing image files
+    config.module.rules.push({
+      test: /\.(png|jpg|jpeg|gif|webp|avif|ico|svg)$/i,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,10 +22,17 @@ export default function Home(): React.ReactElement {
       </Head>
 
       <main className={styles.main}>
-        <Image src={LoginBg} alt='background image' placeholder='blur' layout='fill' className={styles.main__bgImage} />
+      <Image 
+          src="/assets/loginBg.jpg"
+          alt="Login background" 
+          layout="fill"
+          objectFit="cover"
+          placeholder="blur"
+          blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDABQODxIPDRQSEBIXFRQYHjIhHhwcHj0sLiQySUBMS0dARkVQWnNiUFVtVkVGZIhlbXd7gYKBTmCNl4x9lnN+gXz/2wBDARUXFx4aHjshITt8U0ZTfHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHz/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
+        />
         <div className={styles.main__card}>
           <h1>
-            Nextflix
+            Aiflix
           </h1>
           <p>A simple Netflix clone built using Next.js</p>
           <div className={styles.button} onClick={onSignIn}>Sign in</div>
